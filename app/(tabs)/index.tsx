ea26b365ecfbadfdae0f2b9497dcee09bd1743bb
Clint <clint4life@gmail.com>
2024-04-05 19:42:47 -0400

    added ngrok server to get data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,39 @@
-import { Button, StyleSheet } from "react-native";
-import { Text, View } from "react-native";
-import { Link, Stack } from "expo-router";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Button, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
 export default function TabOneScreen() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://2d3c-68-193-89-107.ngrok-free.app');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const handleClearStorage = async () => {
     try {
       await AsyncStorage.clear();
-      console.log("AsyncStorage cleared");
+      console.log('AsyncStorage cleared');
     } catch (error) {
-      console.error("Error clearing AsyncStorage:", error);
+      console.error('Error clearing AsyncStorage:', error);
     }
   };
 
   return (
     <View style={styles.container}>
+      <Text>Data: {JSON.stringify(data)}</Text>
       <Text style={styles.title}>Tab One</Text>
       <Link href="/(pages)/cars">Car List</Link>
-      <Link href="/(pages)">Rent Screen</Link>
       <Button title="Clear AsyncStorage" onPress={handleClearStorage} />
       <Stack>
         <Stack.Screen name="car" />
@@ -29,17 +45,17 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     gap: 10,
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
 });
